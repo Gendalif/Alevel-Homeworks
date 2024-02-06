32e63aa4d73b40c9296609ee43f4238d422d037c
Gendalif <mamaev.seryozha2015@gmail.com>
2024-02-06 18:19:12 +0100

    Blue Belt homework

--- homework/js/01/FirstHomework.js
+++ homework/js/01/FirstHomework.js
@@ -2,10 +2,10 @@ var counterBreadInDay = 5;
 var breadCost = 20;
 var days = 10;
 var consumedBreadInNDays = function (counterBreadInDay, days) {
-    return counterBreadInDay * days;
+  return counterBreadInDay * days;
 };
 var spendMoneyOnBread = function () {
-    return consumedBreadInNDays(counterBreadInDay, days) * breadCost;
+  return consumedBreadInNDays(counterBreadInDay, days) * breadCost;
 };
 var result = spendMoneyOnBread();
 console.log("Result", result);
