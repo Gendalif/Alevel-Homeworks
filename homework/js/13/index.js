7e7f13359a0cad980f800b93416e4ef546bcbe6a
Gendalif <mamaev.seryozha2015@gmail.com>
2024-02-10 00:41:41 +0100

    Update with last confirm pasword form logic

--- homework/js/13/index.js
+++ homework/js/13/index.js
@@ -25,12 +25,12 @@ const submitButton = document.createElement("button");
 submitButton.textContent = "Login";
 submitButton.disabled = true;
 
-function checkInputs() {
+const checkInputs = () => {
   const loginValue = loginInput.getValue().trim();
   const passwordValue = passwordInput.getValue().trim();
 
   submitButton.disabled = !(loginValue && passwordValue);
-}
+};
 
 loginInput.onChange = checkInputs;
 passwordInput.onChange = checkInputs;
@@ -38,3 +38,37 @@ passwordInput.onChange = checkInputs;
 document.body.appendChild(submitButton);
 
 const loginForm = new LoginForm(document.body);
+
+const passwordInput1 = new Input(document.body, false, "Password");
+const passwordInput2 = new Input(document.body, false, "Confirm Password");
+
+const checkPasswords = () => {
+  const password1 = passwordInput1.getValue();
+  const password2 = passwordInput2.getValue();
+
+  if (password1 !== password2) {
+    passwordInput1.input.style.border = "2px solid red";
+    passwordInput2.input.style.border = "2px solid red";
+  } else {
+    passwordInput1.input.style.border = "2px solid green";
+    passwordInput2.input.style.border = "2px solid green";
+  }
+};
+
+passwordInput1.onChange = checkPasswords;
+passwordInput2.onChange = checkPasswords;
+
+const isPasswordOneOpened = passwordInput1.getOpen();
+if (isPasswordOneOpened) {
+  passwordInput2.input.style.display = "none";
+} else {
+  passwordInput2.input.style.display = "inline-block";
+}
+
+passwordInput1.onOpenChange = function (open) {
+  if (open) {
+    passwordInput2.input.style.display = "none";
+  } else {
+    passwordInput2.input.style.display = "inline-block";
+  }
+};
